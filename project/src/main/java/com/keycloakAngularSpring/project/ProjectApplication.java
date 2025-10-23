184accd67b18952583dfd59c8a6b8a4dcbf96047
HoTienQuoc <hotienquoc0429@gmail.com>
2025-10-23 10:39:52 +0700

    Update project configuration: upgraded Java version to 25 in pom.xml, added hierarchical package presentation in VSCode settings, enabled JPA auditing in ProjectApplication, and introduced application-dev.yml for development environment settings while removing application.properties.

--- project/src/main/java/com/keycloakAngularSpring/project/ProjectApplication.java
+++ project/src/main/java/com/keycloakAngularSpring/project/ProjectApplication.java
@@ -2,8 +2,11 @@ package com.keycloakAngularSpring.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableJpaAuditing  // ✅ Correct spelling
 public class ProjectApplication {
 
 	public static void main(String[] args) {
