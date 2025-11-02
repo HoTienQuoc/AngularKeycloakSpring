f947caaae3deb6652a3a684384a5f4d07613ac85
HoTienQuoc <hotienquoc0429@gmail.com>
2025-11-02 20:33:27 +0700

    Add login route and styles: implement login route in app.routes.ts and enhance styles.scss with a new login container and cursor pointer styles for improved UI experience.

---
+++ book-ui/src/app/pages/login/login-component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { AuthenticationRequest } from '../../services/models/authentication-request';
+import { FormsModule } from '@angular/forms';
+import {KeycloakService} from '../../services/keycloak/keycloak.service';
+
+@Component({
+  selector: 'app-login-component',
+  imports: [FormsModule],
+  templateUrl: './login-component.html',
+  styleUrl: './login-component.scss',
+})
+export class LoginComponent {
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private ss: KeycloakService
+  ) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.ss.init();
+    await this.ss.login();
+  }
+
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.errorMsg);
+        }
+      }
+    });
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+}
